472a5a51390fb939a291d2d829ed43074e6d92c4
shiki <shiki@mbp.local>
2018-11-08 21:47:38 +0900

    add page

--- express-app/index.js
+++ express-app/index.js
@@ -10,7 +10,17 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.render("index.ejs", {
     title: "Index",
-    content: "This is Express-app Top page"
+    content: "This is Express-app Top page",
+    link: { href: "/other", text: "別ページへ移動" }
+  });
+});
+
+app.get("/other", (req, res) => {
+  var msg = "This is Other Page";
+  res.render("index.ejs", {
+    title: "other",
+    content: msg,
+    link: { href: "/", text: "トップに戻る" }
   });
 });
 
