eaef709b6c526796ceadc7eac518582a39f8fc66
shiki <shiki@mbp.local>
2018-11-08 21:12:59 +0900

    add ejs

--- express-app/index.js
+++ express-app/index.js
@@ -1,10 +1,16 @@
 var express = require('express')
+var ejs = require('ejs')
+
 var app = express()
 
+app.engine('ejs', ejs.renderFile)
+
 app.get('/', (req, res) => {
-  res.send('Hello Express!')
+  res.render('index.ejs',
+    {title: 'Index',
+      content: 'This is Express-app Top page'})
 })
 
 app.listen(3000, ()=>{
-  console.log('Start server port 3000')
+  console.log('Server is running!')
 })
