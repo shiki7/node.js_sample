212598918fcd13b0dceb15b30c25235191b9d525
shiki <shiki@mbp.local>
2018-11-08 21:31:40 +0900

    add css

--- express-app/index.js
+++ express-app/index.js
@@ -1,16 +1,19 @@
-var express = require('express')
-var ejs = require('ejs')
+var express = require("express");
+var ejs = require("ejs");
 
-var app = express()
+var app = express();
 
-app.engine('ejs', ejs.renderFile)
+app.engine("ejs", ejs.renderFile);
 
-app.get('/', (req, res) => {
-  res.render('index.ejs',
-    {title: 'Index',
-      content: 'This is Express-app Top page'})
-})
+app.use(express.static("public"));
 
-app.listen(3000, ()=>{
-  console.log('Server is running!')
-})
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
+    title: "Index",
+    content: "This is Express-app Top page"
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running!");
+});
