4f0b7f1478275c0a52072c047ff088d83719fc6e
shiki <shiki@mbp.local>
2018-11-08 22:18:40 +0900

    get

--- express-app/index.js
+++ express-app/index.js
@@ -11,12 +11,15 @@ app.get("/", (req, res) => {
   res.render("index.ejs", {
     title: "Index",
     content: "This is Express-app Top page",
-    link: { href: "/other", text: "別ページへ移動" }
+    link: { href: "/other?name=taro&pass=yamada", text: "別ページへ移動" }
   });
 });
 
 app.get("/other", (req, res) => {
-  var msg = "This is Other Page";
+  var name = req.query.name;
+  var pass = req.query.pass;
+  var msg =
+    "あなたの名前は「" + name + "」<br>" + "パスワードは「" + pass + "」です。";
   res.render("index.ejs", {
     title: "other",
     content: msg,
