08af98edf759f8d025c4946afbce5f7623e1b0cc
shiki <shiki@mbp.local>
2018-11-09 23:51:47 +0900

    table

--- express-app/index.js
+++ express-app/index.js
@@ -10,11 +10,18 @@ app.use(express.static("public"));
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 
+var data = {
+  Taro: "taro@hoge",
+  Hanako: "hanako@hoge",
+  Sachiko: "sachico@hoge"
+};
+
 app.get("/", (req, res) => {
   var msg = "メッセージを書いてください";
   res.render("index.ejs", {
     title: "Index",
-    content: msg
+    content: msg,
+    data: data
   });
 });
 
