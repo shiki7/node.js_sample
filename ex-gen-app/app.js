62a1e184836df41bd55300011a8b4cd53bc723a7
shiki <shiki@mbp.local>
2018-11-10 13:50:19 +0900

    ajax

--- ex-gen-app/app.js
+++ ex-gen-app/app.js
@@ -7,6 +7,7 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var helloRouter = require("./routes/hello");
+var ajax = require("./routes/ajax");
 
 var session = require("express-session");
 
@@ -34,6 +35,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/hello", helloRouter);
+app.use("/ajax", ajax);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
