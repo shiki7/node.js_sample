9ac6f658591b49bdcd1b8a2a20dc9026bbcfb18f
shiki <shiki@mbp.local>
2018-11-10 12:17:47 +0900

    add post

--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -3,9 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
+  // getパラメータ取得
+  // var name = req.query.name;
+  // var mail = req.query.mail;
   var data = {
     title: "Hello!",
-    content: "sample contents"
+    // content: "your_name=" + name + "<br>your_mail_adress=" + mail
+    content: "なにか入力してください"
+  };
+  res.render("hello", data);
+});
+
+router.post("/post", function(req, res, next) {
+  // postを取得
+  var msg = req.body["message"];
+  var data = {
+    title: "Hello!",
+    content: msg
   };
   res.render("hello", data);
 });
